416aafc5406756448a27f8180d7e3dd384508d5b
Andrew Nesbitt <andrewnez@gmail.com>
2016-12-29 13:50:57 +0000

    It's working

--- org_pulse.rb
+++ org_pulse.rb
@@ -1,11 +1,28 @@
 require 'bundler'
 Bundler.require
+Dotenv.load
 
-# find all repos in a org
+org = 'librariesio'
+access_token = ENV['GITHUB_TOKEN']
+client = Octokit::Client.new(access_token: access_token, auto_paginate: true)
+start_date = '2016-12-01'
+end_date = '2016-12-31'
 
-# for each repo:
+repos = client.org_repos(org)
 
-  # download issues created/updated within the past month
-  # download pull requests created/updated within the past month
-  # download commits within the past month
-  # find repos created this month
+repos.sort_by(&:stars).reverse.each do |repo|
+  issues = client.issues(repo.full_name, state: 'all')
+  pull_requests = client.pull_requests(repo.full_name, state: 'all')
+  commits = client.commits(repo.full_name, since: start_date, until: end_date) rescue []
+
+  closed_issues = issues.select{|i| i.closed_at && i.closed_at > Date.parse(start_date).to_time && i.closed_at < Date.parse(end_date).to_time }
+  merged_pull_requests = pull_requests.select{|i| i.merged_at && i.merged_at > Date.parse(start_date).to_time && i.merged_at < Date.parse(end_date).to_time }
+
+  if commits.length > 0 || merged_pull_requests.length > 0 || closed_issues.length > 0
+    puts repo.name
+    puts "  #{commits.length} commits" if commits.length > 0
+    puts "  #{merged_pull_requests.length} merged pull requests" if merged_pull_requests.length > 0
+    puts "  #{closed_issues.length} closed issues" if closed_issues.length > 0
+    puts
+  end
+end
