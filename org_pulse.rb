16c431d6431c7e7ea24b5ab5c167b13304fa1f96
Andrew Nesbitt <andrewnez@gmail.com>
2017-03-31 13:18:36 +0100

    Update month

--- org_pulse.rb
+++ org_pulse.rb
@@ -7,7 +7,7 @@ require 'active_support/core_ext/time/calculations'
 # Override org and dates to customize
 org = 'librariesio'
 year = 2017
-month = 01
+month = 03
 start_date = Date.civil(year, month, 1)
 end_date = start_date.end_of_month
 
